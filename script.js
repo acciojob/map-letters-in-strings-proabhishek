dfd99de3dc3ec84b5a289a3e537d6f459504fc5b
Abhishek Shankar Choudhary <28715657+proabhishek@users.noreply.github.com>
2024-04-11 22:24:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+function mapLetters(str){
+  let obj = {}
+  for(let i = 0; i<str.length; i++){ 
+      let t = str[i] // d, o, d, o
+        if(obj[t] == undefined){
+            obj[t] = [i]  // {d: [0,2], o: [1,3]}
+        }
+        else{
+            let arr = obj[t] // [0,2]
+            arr.push(i)
+        }
+  }
+
+    return obj
+}
+
+
+const str = prompt("Enter str: ");
+alert(JSON.stringify(mapLetters(str)));
